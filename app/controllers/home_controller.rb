32d0994a28eb720a9a5b4f84c27aa37b2de26a51
anromeo <anthonykzullo@gmail.com>
2013-06-24 19:25:58 -0700

    fixed the broken header and added partials

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
 	def index
+    if current_user && current_user.mentor
+      redirect_to mentors_path
+    elsif current_user && current_user.student
+      redirect_to students_path
+    end
 	end
 end
